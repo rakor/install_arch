8e986e375a4cd8c026a585687a7fb4455bfb59be
rakor <git@rakor.de>
2023-01-27 15:01:50 +0100

    lets try something

--- archinstall.sh
+++ archinstall.sh
@@ -3,10 +3,58 @@
 USERNAME=rakor
 HDD=/dev/vda
 MYHOSTNAME=archlinux
-ADDITIONALPACKETS="man-pages-de syncthing ufw"
-SWAPGB=2
+ADDITIONALPACKETS="man-pages-de syncthing ufw restic git fish unzip gnome gnome-extra"
+ENABLESERVICES="NetworkManager ufw gdm"
+SWAPGB=1
 EFIGB=1
 
+# Put in additional commands to run as root after the installation has
+# finished inside the new environment
+additionalCommands(){
+    ufw enable
+    ufw allow syncthing
+    chsh -s /usr/bin/fish $USERNAME	
+    
+    curl https://github.com/rakor/resticbackupscript/archive/master.zip -o /root/backupscript.zip
+    unzip backupscript.zip
+    rm backupscript.zip
+    sh resticbackupscript-master/install.sh
+    sed -e "s[^\s*RESTIC=.*\$[RESTIC=/usr/bin/restic[" resticbackupscript-master/resticrc_debian > /root/.resticrc
+    rm -rf resticbackupscript-master
+    echo "#!/bin/sh\n/usr/local/bin/resticbackup cron" > /etc/cron.hourly/backup
+    chmod 755 /etc/cron.hourly/backup
+
+    ####
+    # vimrc
+    curl -s
+    https://raw.githubusercontent.com/rakor/config/master/home/.vimrc -o /root/.vimrc
+    chown root:root /root/.vimrc
+    chmod 644 /root/.vimrc
+    https://raw.githubusercontent.com/rakor/config/master/home/.vimrc -o /home/$USERNAME/.vimrc
+    chown $USERNAME:$USERNAME /home/$USERNAME/.vimrc
+    chmod 644 /home/$USERNAME/.vimrc
+    mkdir -p /root/.vim/colors
+    mkdir -p /home/$USERNAME/.vim/colors
+    curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o /home/$USERNAME/.vim/colors/molokai.vim
+    cp /home/$USERNAME/.vim/colors/molokai.vim /root/.vim/colors/molokai.vim
+    chown $USERNAME:$USERNAME -R /home/$USERNAME/.vim
+
+    echo "RESTIC"
+    echo "======"
+    echo "Please don't forget to set repository and password for the restic-backups in /root/.resticrc."
+    echo "Then you have to 'resticcmd init' the repository if it is a new one."	
+    
+    echo "Syncthing"
+    echo "========="
+    echo "If you want to start syncthing automatically at logon of your"
+    echo "user run as user $USERNAME:"
+    echo "  systemctl --user enable syncthing.service"
+    echo "  systemctl --user start syncthing.service"
+    echo "Syncthing will be listening on Port 8384 for the Webinterface"
+    echo "If you also want to allow external access to the Syncthing web GUI, run:"
+    echo "  ufw allow syncthing-gui"
+    echo "Allowing external access is not necessary for a typical installation."
+}
 
 # Datei die den Installationsstatus haelt
 STATUSFILE=~/archinstallservice.txt
@@ -23,33 +71,33 @@ nextstep(){
 }
 
 step1(){
-#German keyboardlayout
+	#German keyboardlayout
     loadkeys de-latin1
 
-# Test network
+	# Test network
     timedatectl status
 
-# Partitioning
-# Create a GPT-layout and inside an efs-partition, swap, and root-pool
+	# Partitioning
+	# Create a GPT-layout and inside an efs-partition, swap, and root-pool
     parted $HDD mklabel gpt
     parted $HDD mkpart efi-part fat32 1MiB ${EFIGB}GiB
     parted $HDD set 1 esp on
-# create a 2GB swap partition (Important: the last value is the
-# endpoint, not the size!
+	# create a 2GB swap partition (Important: the last value is the
+	# endpoint, not the size!
     parted $HDD mkpart swap linux-swap 1GiB $(($EFIGB+$SWAPGB))GiB
-# rest for the pool
+	# rest for the pool
     parted $HDD mkpart zpool 3GiB 100%
 
     mkswap /dev/disk/by-partlabel/swap
 
     mkfs.fat -F 32 /dev/disk/by-partlabel/efi-part
 
-# Install zfs into the install-environment
+	# Install zfs into the install-environment
     curl -s https://raw.githubusercontent.com/eoli3n/archiso-zfs/master/init | bash
     modprobe zfs
 
     echo "Creating the ZFS-pool. Give your encryption-key"
-# create the zpool
+	# create the zpool
     zpool create \
     -o ashift=12 \
     -o autotrim=on \
@@ -68,7 +116,7 @@ step1(){
     -R /mnt \
     zroot /dev/disk/by-partlabel/zpool
 
-# create the datasets
+	# create the datasets
     zfs create -o mountpoint=none zroot/data
     zfs create -o mountpoint=none zroot/ROOT
     zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/default
@@ -85,44 +133,36 @@ step1(){
     zfs create -o com.sun:auto-snapshot=false zroot/data/home/${USERNAME}/Downloads
     zfs create -o com.sun:auto-snapshot=false zroot/data/home/$USERNAME/${USERNAME}-home
 
-# export the pool
+	# export the pool
     zpool export zroot
-# reimport the pool
+	# reimport the pool
     zpool import -d /dev/disk/by-partlabel -R /mnt zroot -N
-# load the encryption-key
+	# load the encryption-key
     echo;echo
     echo "Type your ZFS-encryption-key to unlock the pool"
     zfs load-key zroot
 
-# mount the datasets
+	# mount the datasets
     zfs mount zroot/ROOT/default
     zfs mount -a
 
-# prepare the pool to boot
+	# prepare the pool to boot
     zpool set bootfs=zroot/ROOT/default zroot
     zpool set cachefile=/etc/zfs/zpool.cache zroot
     mkdir -p /mnt/etc/zfs
     cp -p /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
 
 
-# installation of arch-linux
+	# installation of arch-linux
     mkdir -p /mnt/boot/efi
     mount /dev/disk/by-partlabel/efi-part /mnt/boot/efi
     swapon /dev/disk/by-partlabel/swap
     pacstrap -K /mnt base linux linux-firmware vim networkmanager
     genfstab -U /mnt >> /mnt/etc/fstab
 
-# remove the zfs-datasets vom fstab
+	# remove the zfs-datasets vom fstab
     sed -i -e 's|^\(\S*\s\+\S*\s\+zfs\)|#\1|' /mnt/etc/fstab
     
-# set permissions
-    cp -r /mnt/etc/skel/.[^.]* /mnt/home/$USERNAME
-    cp -r /mnt/etc/skel/* /mnt/home/$USERNAME
-    chown -R $USERNAME:$USERNAME /mnt/home/$USERNAME
-    chmod 700 /mnt/root
-    chmod 1777 /mnt/tmp
-    chmod 700 /mnt/home/$USERNAME
-
     nextstep
     cp $0 /mnt/root/
     cp $STATUSFILE /mnt/root
@@ -137,22 +177,22 @@ step1(){
 }
 
 step2(){
-# basesettings
+	# basesettings
     ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
     hwclock --systohc
 
-# prepare locale.gen to get german an us and set language-settings
+	# prepare locale.gen to get german an us and set language-settings
     sed -i -e 's%^#s*\(\(en_US\|de_DE\)\.UTF-8\)%\1%' /etc/locale.gen
     locale-gen
     echo LANG=de_DE.UTF-8 >> /etc/locale.conf
     echo KEYMAP=de-latin1 >> /etc/vconsole.conf
     echo $MYHOSTNAME >> /etc/hostname
 
-# prepare mkinitcpio
+	# prepare mkinitcpio
     sed -i -e 's|^\s*\(HOOKS=.*\)\(filesystems.*\)|\1 zfs \2|' /etc/mkinitcpio.conf
     sed -i -e 's|^\s*\(HOOKS=.*\)fsck\(.*\)|\1 \2|' /etc/mkinitcpio.conf
 
-# install zfs into the new installation
+	# install zfs into the new installation
     echo [archzfs] >> /etc/pacman.conf
     echo 'Server = http://archzfs.com/$repo/x86_64' >> /etc/pacman.conf
     echo 'Server = http://mirror.sum7.eu/archlinux/archzfs/$repo/x86_64' >> /etc/pacman.conf
@@ -162,11 +202,11 @@ step2(){
     pacman-key --lsign-key DDF7DB817396A49B2A2723F7403BD972F75D9D76
     pacman -S --noconfirm zfs-dkms linux-headers
 
-#create the initrd
-#    mkinitcpio -P
+	#create the initrd
+	#    mkinitcpio -P
 
 
-# install bootmanager zbm
+	# install bootmanager zbm
     pacman -S --noconfirm efibootmgr
     mkdir -p /boot/efi/EFI/zbm
     curl -L https://get.zfsbootmenu.org/efi/release -o /boot/efi/EFI/zbm/zfsbootmenu.EFI
@@ -175,7 +215,7 @@ step2(){
 
     zpool set cachefile=/etc/zfs/zpool.cache zroot
 
-# start and mount zfs
+	# start and mount zfs
     systemctl enable zfs.target
     systemctl enable zfs-import-cache.service
     systemctl enable zfs-mount.service
@@ -183,24 +223,45 @@ step2(){
     zgenhostid $(hostid)
     mkinitcpio -P
 
-# Set password for root
+	# Set password for root
     echo;echo
     echo "Please set password for 'root'"
     passwd
+    mv /root/.bashrc /root/.bashrc_old
+    curl -s  https://raw.githubusercontent.com/rakor/config/master/root/.bashrc -o /root/.bashrc
 
-# last steps to prepare the installation
+	# last steps to prepare the installation
 
-# Create user and its datasets
+	# Create user and its datasets
     echo;echo
     echo "Creating user $USERNAME"
     useradd -m -G wheel $USERNAME
     echo "Please give the password to set for user $USERNAME"
     passwd $USERNAME
 
-# install gnome
-    pacman -S --noconfirm gnome gnome-extra
-    systemctl enable gdm.service
-    systemctl enable NetworkManager.service
+	# set permissions
+    cp -r /etc/skel/.[^.]* /home/$USERNAME
+    cp -r /etc/skel/* /home/$USERNAME
+    chown -R $USERNAME:$USERNAME /home/$USERNAME
+    chmod 700 /root
+    chmod 1777 /tmp
+    chmod 700 /home/$USERNAME
+
+
+	#install additional packages
+    pacman -S --noconfirm $ADDITIONALPACKETS
+
+	#enable services
+    for i in $ENABLESERVICES; do 
+        systemctl enable $i.service
+
+    done
+    
+	#run additional commands that were defined at the top
+    echo "Running additional commands"
+    additionalCommands
+
+    rm /root/$STATUSFILE
 
     echo;echo
     echo "You can now leave the chroot. Please do the following:"
@@ -211,7 +272,7 @@ step2(){
     echo "reboot"
 
     nextstep
-# We should somehow install the microcode-updates...
+	# We should somehow install the microcode-updates...
         exit
 }
 
